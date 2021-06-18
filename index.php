28cf7f68dd28394d495fe449082673eeca3feca6
Anup Kumar <kumaranup594@gmail.com>
2021-06-18 07:13:22 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Plugin Name: Send Password Instead of Email
+ * Version:     1.0.0
+ * Plugin URI:  https://www.instagram.com/ianup.nexgi/
+ * Description: This plugin is specially created for one of my client
+ * Author:      Anup Kumar
+ * Author URI:  https://www.instagram.com/ianup.nexgi/
+ *
+ * NexGen Innovators is New Delhi, India based IT company provides quality solution to wordPress clients over the world.
+ *
+ */
+add_filter( 'wp_new_user_notification_email', 'custom_wp_new_user_notification_email', 100000, 3 );
+function custom_wp_new_user_notification_email( $wp_new_user_notification_email, $user, $blogname ) {
+
+    if(in_array('subscriber', $user->roles)) {
+        $user_id = $user->ID;
+        $password = randomPassword();
+        wp_set_password($password, $user_id);
+        $message = 'Hey ' . $user->display_name . ', Welcome to our website' . "\r\n\r\n";
+        $message .= "Please find the username password to login." . "\r\n";
+        $message .= "username - " . $user->user_login . "\r\n";
+        $message .= "Password - " . $password . "\r\n";
+        $message .= "After this you can enjoy our website!" . "\r\n\r\n";
+        $message .= "Thanks" . "\r\n";
+        $message .= "Support Team | " . $blogname . "\r\n";
+        $wp_new_user_notification_email['message'] = $message;
+    }
+    return $wp_new_user_notification_email;
+}
+if(!function_exists('randomPassword')) {
+    function randomPassword()
+    {
+        $alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
+        $pass = array(); //remember to declare $pass as an array
+        $alphaLength = strlen($alphabet) - 1; //put the length -1 in cache
+        for ($i = 0; $i < 8; $i++) {
+            $n = rand(0, $alphaLength);
+            $pass[] = $alphabet[$n];
+        }
+        return implode($pass); //turn the array into a string
+    }
+}
---
+++ readme.md
@@ -0,0 +1,31 @@
+# Send Password Instead of Email WordPress Plugin
+This plugin helps you to send password instead of reset links. 
+
+## Problem Statement
+When we create a new user in wordpress. Wordpress send a link to added user's email. There is no provision to send password by default.
+
+## Solution Statement
+This plugin will generate a random password and sends to their email.
+
+## What is not possible
+If you want to send your added password on user's email. This is not possible because wordpress use MD5 (one way encryption technique).
+
+So nobody can decrypt that and send your added password while you have created user.
+
+## How to use Send Password Instead of Email WordPress Plugin
+- Download this plugin
+- upload in plugin directory
+- activate it
+- nothing else is required to do.
+
+## About me
+I'm Anup, A Full stack PHP Developer based in Delhi, India. I'm comfortable with any CMS & Framework based on PHP or LAMP stack.
+
+Few Top Framework & CMS
+- Laravel
+- CodeIgniter
+- WordPress
+- Drupal
+- Zend
+- CakePHP
+- Opencart
